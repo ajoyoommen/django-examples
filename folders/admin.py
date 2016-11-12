ed0f6fcaecf60d90d45f9477fef2ec7248fd1704
Ajoy Oommen <ajoy91ad@gmail.com>
2016-11-12 17:51:26 +0530

    Add FolderAdmin for Folder

--- folders/admin.py
+++ folders/admin.py
@@ -1,1 +1,26 @@
 from django.contrib import admin
+
+from folders.models import Folder
+
+
+class FolderInline(admin.TabularInline):
+    model = Folder
+    readonly_fields = ['path']
+    show_change_link = True
+
+
+class FolderAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': (('name', 'parent'), 'path')
+        }),
+    )
+    inlines = [FolderInline]
+    list_display = ['path', 'parent']
+    raw_id_fields = ['parent']
+    readonly_fields = ['path']
+    search_fields = ['name', 'path']
+
+
+
+admin.site.register(Folder, FolderAdmin)
